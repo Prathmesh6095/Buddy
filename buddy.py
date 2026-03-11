d4cd5d0031a26cb46a248f44aade027584028a64
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-11 14:12:42 +0530

    Add email section

--- buddy.py
+++ buddy.py
@@ -31,3 +31,38 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
 from buddyui import Ui_Dialog
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id)
+engine.setProperty('voices', voices[0].id)
+
+#text to speech
+def speak(audio):
+    engine.say(audio)
+    print(audio)
+    engine.runAndWait()
+
+#to wish
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    pt = time.strftime("%I:%M %p")
+    if hour>=0 and hour<12:
+        speak(f"Good morning sir!, its {pt}")
+
+    elif hour>=12 and hour<18:
+        speak(f"Good Afternoon sir!, its {pt}")
+
+    else:
+        speak(f"Good evening sir!, its {pt}")
+    speak("I am Buddy , How can I help you?")
+
+#to send email
+def sendEmail(to,content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('prathameshhatkar07@gmail.com','tkne adgo kwnp jwbt')
+    server.sendmail('prathameshhatkar07@gmail.com',to,content)
+    server.close()
