a5b70c01d2583cdfd0bea0be4ea70ef8df01a836
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-11 13:59:24 +0530

    Add packages first

---
+++ buddy.py
@@ -0,0 +1,33 @@
+import pyttsx3
+import datetime
+from time import sleep
+import speech_recognition as sr
+import os
+import cv2
+import random
+from requests import get
+import requests
+import wikipedia
+import webbrowser
+import pywhatkit as kit
+import smtplib 
+import sys
+import time
+import pyjokes
+import instaloader
+import pyautogui
+import PyPDF2
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+import instaloader
+import operator
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import QTimer, QTime, QDate
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+from buddyui import Ui_Dialog
